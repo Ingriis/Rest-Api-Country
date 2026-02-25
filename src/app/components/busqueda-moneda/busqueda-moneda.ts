fc2e8b594e54fbc26e5a2d9cdfb54f19bdfdcc6d
soykarencoronaa <l22200722@pachuca.tecnm.mx>
2026-02-24 23:08:36 -0600

    Prueba endpoint buscador-moneda

---
+++ src/app/components/busqueda-moneda/busqueda-moneda.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda-moneda',
+  imports: [],
+  templateUrl: './busqueda-moneda.html',
+  styleUrl: './busqueda-moneda.css',
+})
+export class BusquedaMoneda {
+
+}
